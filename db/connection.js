ecfe0794263df99c8eb80f958f520a34d0bc3283
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-10 10:00:37 -0500

    saving

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3000,
+    user: root,
+    password: "password",
+    database: "employeeDB"
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
---
+++ db/employeeDB.sql
@@ -0,0 +1,79 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+)
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL UNSIGNED,
+    INDEX role_id (role_id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    -- foreign key references cascade 
+    manager_id INT UNSIGNED,
+    INDEX man_id (manager_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    -- foreign key references cascade 
+)
+
+CREATE TABLE role (
+    department_id INT NOT NULL UNSIGNED,
+    INDEX dep_id (department_id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    -- foreign key references cascade 
+
+    title VARCHAR(30),
+    salary INT NOT NULL UNSIGNED
+)
+
+INSERT INTO department (name)
+VALUES ("Sales");
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+INSERT INTO department (name)
+VALUES ("Finance");
+
+INSERT INTO department (name)
+VALUES ("Legal");
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ()
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ()
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ()
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ()
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ()
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineering");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Finance");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal");
+
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
